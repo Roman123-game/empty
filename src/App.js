f56d6fce0236317447ee598e1c617fc9c859d5e1
RB <82284311+Roman123-game@users.noreply.github.com>
2023-12-01 17:46:28 +0200

    update App.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import logo from './logo.svg';
+import { useState , useEffect } from 'react';
 import './App.css';
-import { useEffect } from 'react';
 
-const object ={
-  name: "alex",
-  age :40 
-}
+// const object ={
+//   name: "alex",
+//   age :40 
+// }
 
 
-function createSet(){
-  const array = ['a','b', 'c', 'b','a']
-  const set = Array.from(new Set(array).values())
 
- console.log(set)
-
-  }
   
 
-function createFromObj(){
-const keys = Object.keys(object)
-}
+// function createFromObj(){
+// const keys = Object.keys(object)
+// }
 
 
 
 
 function App() {
+  const [widthh, setWidthh]= useState(0)
+  const offsetWidth = document?.getElementById('pix')?.offsetWidth;
+
+  useEffect(() => {
+    console.log("offsetWidth",offsetWidth);
+    setWidthh(offsetWidth)
+    
+}, [widthh])
   function InnerWidth(){
-    const offsetWidth = document?.getElementById('pix')?.offsetWidth;
-      useEffect(() => {
-        console.log("offsetWidth",offsetWidth);
-        
-  }, [offsetWidth])
-  return <div>{offsetWidth}</div>
+
+  return <div>{widthh}</div>
   }
  
+
+  function createSet(){
+    const array = ['a','b', 'c', 'b','a']
+    const set = Array.from(new Set(array).values())
+  
+   console.log(set)
+  return <div>{set}</div>
+    }
 
   // useEffect(() => {
  
@@ -43,7 +48,8 @@ function App() {
     <div className="App">
     <div id="pix"></div>
     <InnerWidth className="width"/>
-   <button onClick={createSet}>clcick</button>
+
+   <button onClick={createSet}>{createSet()}</button>
     </div>
   );
 }
